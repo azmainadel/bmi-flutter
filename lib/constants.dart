1113fffd15dea054e78ca4ab214344d9a5429f53
Azmain Adel <azmainadel47@gmail.com>
2020-01-07 13:18:54 +0600

    Add constants file

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberlTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kBottomContainerHeight = 75.0;
+const kBottomContainerColor = Color(0xFFEB1555);
+
+const kActiveCardColor = Color(0xFF1D1E33);
+const kInactiveCardColor = Color(0xFF111328);
