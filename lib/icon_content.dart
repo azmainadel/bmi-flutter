7b5333eb24d1f83c8e8d08619582dd9d9f3ded35
Azmain Adel <azmainadel47@gmail.com>
2020-01-07 13:18:45 +0600

    Add slider

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
+
 class IconContent extends StatelessWidget {
   final IconData icon;
   final String iconText;
@@ -20,10 +22,7 @@ class IconContent extends StatelessWidget {
         ),
         Text(
           iconText,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
+          style: kLabelTextStyle,
         )
       ],
     );
