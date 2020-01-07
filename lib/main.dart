47a31c5b0e176f81f803c098d2965f3d7e529d1d
Azmain Adel <azmainadel47@gmail.com>
2020-01-07 19:05:08 +0600

    Add screenshots and remove debug mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class BMICalculator extends StatelessWidget {
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
+      debugShowCheckedModeBanner: false,
       initialRoute: '/input',
       routes: {
         '/input': (context) => InputPage(),
